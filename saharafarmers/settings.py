634f369962f1c575c9a93a1405775da615ebdcbd
bioscom <bioscomsoft@gmail.com>
2019-12-01 00:33:50 +0100

    Added files and changes required for deployment to heroku

--- saharafarmers/settings.py
+++ saharafarmers/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-## SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'm+civq7q#_1nqxb@@@8hk&@(ss5l0ht1f3h-&b&a4%qjhtilo2'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'm+civq7q#_1nqxb@@@8hk&@(ss5l0ht1f3h-&b&a4%qjhtilo2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
